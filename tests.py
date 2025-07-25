2c0d3112b3e3fe2f5b06491276ef1a33b01d9547
Hanna Radzevich <hradzevich@yandex.com>
2025-07-25 15:11:04 +0200

    Добавлены фикстуры для создания пустого объекта класса и объекта класса с добавленными книгами и установленными жанрами, внесены изменения в test_add_new_book_add_two_books с учетом фикстуры collector и из-за ошибки в assert, добавлен позитивный параметризированный тест для set_book_genre

--- tests.py
+++ tests.py
@@ -1,24 +1,51 @@
+import pytest
 from main import BooksCollector
 
+
+# создаем фикстуру для создания экземпляра класса
+@pytest.fixture
+def collector():
+    return BooksCollector()
+
+
+# создаем фикстуру для создания экземпляра класса с коллекцией книг с присвоенными им жанрами
+@pytest.fixture
+def collector_with_books_and_genres():
+    collector = BooksCollector()
+    collection = [
+        ["Гарри Поттер", "Фантастика"],
+        ["Шерлок Холмс", "Детективы"],
+        ["Том и Джерри", "Мультфильмы"],
+        ["Друзья", "Комедии"],
+    ]
+    for book_name, genre in collection:
+        collector.add_new_book(book_name)
+        collector.set_book_genre(book_name, genre)
+    return collector
+
+
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
-
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
-
+    # тестируем add_two_books - добавление двух книг
+    def test_add_new_book_add_two_books(self, collector):
         # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
+        collector.add_new_book("Гордость и предубеждение и зомби")
+        collector.add_new_book("Что делать, если ваш кот хочет вас убить")
         # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        # словарь books_genre, который нам возвращает метод get_books_genre, имеет длину 2
+        assert len(collector.get_books_genre()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    # параметризация для проверки установки жанра у разных книг
+    @pytest.mark.parametrize(
+        "book_name, genre",
+        [["Гарри Поттер", "Фантастика"], ["Шерлок Холмс", "Детективы"]],
+    )
+    # тестируем set_book_genre - установка жанра у добавленных в books_genre книг
+    def test_set_book_genre_books_from_books_genre(
+        self, collector, book_name, genre
+    ):
+
+        collector.add_new_book(book_name)
+        collector.set_book_genre(book_name, genre)
+
+        assert collector.books_genre[book_name] == genre
