f5959882e3d7dd269dacdbec47954c5c92d7979f
Hanna Radzevich <hradzevich@yandex.com>
2025-07-28 10:01:07 +0200

    Добавлен негативный тест для метода add_new_book с добавлением уже ранее добавленной книги в коллекцию

--- tests.py
+++ tests.py
@@ -12,7 +12,7 @@ def collector():
 @pytest.fixture
 def collector_with_books():
     collector = BooksCollector()
-    book_lst = ["Гарри Поттер", "Шерлок Холмс"]
+    book_lst = ["Гарри Поттер и дары смерти", "Шерлок Холмс. Пустой дом"]
     for book_name in book_lst:
         collector.add_new_book(book_name)
     return collector
@@ -36,19 +36,46 @@ def collector_with_books_and_genres():
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 class TestBooksCollector:
-    #параметризация для проверки добавления книг с валидным количеством символов в названии (1, 2, 39, 40)
-    @pytest.mark.parametrize("book_name", ["Я", "Мы", "Тайны затерянного города среди джунглей", "Тени исчезают в тумане над старым мостом"])
-    # тестируем add_new_book - добавление книг с валидным количеством символов в названии 
+    # параметризация для проверки добавления книг с валидным количеством символов в названии (1, 2, 39, 40)
+    @pytest.mark.parametrize(
+        "book_name",
+        [
+            "Я",
+            "Мы",
+            "Тайны затерянного города среди джунглей",
+            "Тени исчезают в тумане над старым мостом",
+        ],
+    )
+    # тестируем add_new_book - добавление книг с валидным количеством символов в названии
     def test_add_new_book_add_books_with_valid_len(self, collector, book_name):
         collector.add_new_book(book_name)
         assert len(collector.get_books_genre()) == 4
 
-    #параметризация для проверки, что книги с невалидным количеством символов в названии (0, 41, 59) не попадают в коллекцию
-    @pytest.mark.parametrize("book_name", ["", "Проклятие заброшенного дома на краю света", "Тайны древних руин, скрывающихся под покровом ночи и тумана"])
+    # параметризация для проверки, что книги с невалидным количеством символов в названии (0, 41, 59) не попадают в коллекцию
+    @pytest.mark.parametrize(
+        "book_name",
+        [
+            "",
+            "Проклятие заброшенного дома на краю света",
+            "Тайны древних руин, скрывающихся под покровом ночи и тумана",
+        ],
+    )
     # тестируем add_new_book - книги с невалидным количеством символов в названии не добавляются в коллекцию
     def test_add_new_book_add_books_with_invalid_len(self, collector, book_name):
         collector.add_new_book(book_name)
         assert len(collector.get_books_genre()) == 0
+
+    # тестируем add_new_book - книга повторно не добавляется в коллекцию
+    def test_add_new_book_add_added_book(self, collector_with_books_and_genres):
+        book_name = "Гарри Поттер и дары смерти"
+        before_adding_book_again = len(collector_with_books_and_genres.books_genre)
+        collector_with_books_and_genres.add_new_book(book_name)
+        after_adding_book_again = len(collector_with_books_and_genres.books_genre)
+        assert (
+            before_adding_book_again == after_adding_book_again
+            and collector_with_books_and_genres.get_book_genre(book_name)
+            == "Фантастика"
+        )
 
     # параметризация для проверки установки жанра у разных книг добавленных в books_genre
     @pytest.mark.parametrize("genre", ["Фантастика", "Детективы"])
@@ -59,8 +86,7 @@ class TestBooksCollector:
             assert collector_with_books.books_genre[book] == genre
 
     # тестируем set_book_genre - книге не из books_genre жанр из genre не присваивается
-    def test_set_book_genre_books_not_in_books_genre(
-        self, collector_with_books):
+    def test_set_book_genre_books_not_in_books_genre(self, collector_with_books):
         book_name = "Оно"
         genre = "Ужасы"
         collector_with_books.set_book_genre(book_name, genre)
