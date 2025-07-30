034c898ea3fb29d4d92385c54f90a54bb8c30384
Hanna Radzevich <hradzevich@yandex.com>
2025-07-30 10:44:04 +0200

    Внесены изменения в тест test_delete_book_from_favorites_book_not_in_favorites в связи с замечанием по поводу использования фикстур для наполнения словаря, использована фикстура collector и наполнение словаря, добавление в favorites  происходит в самом тесте

--- test_main.py
+++ test_main.py
@@ -289,21 +289,30 @@ class TestBooksCollector:
         for book, genre in books_with_genres.items():
             collector.add_new_book(book)
             collector.set_book_genre(book, genre)
-        collector.add_book_in_favorites("Том и Джерри")
+            collector.add_book_in_favorites(book)
         book_to_delete = "Том и Джерри"
-        before_change_favorites = len(collector.favorites)
         collector.delete_book_from_favorites(book_to_delete)
-        after_change_favorites = len(collector.favorites)
-        assert book_to_delete not in collector.favorites and after_change_favorites == before_change_favorites - 1
+        assert book_to_delete not in collector.favorites
 
     # тестируем delete_book_from_favorites - книга не добавленная в favorites не может быть удалена из favorites
     def test_delete_book_from_favorites_book_not_in_favorites(
-        self, collector_with_favorites
+        self, collector
     ):
+        books_with_genres = {
+            "Гарри Поттер и дары смерти": "Фантастика",
+            "Шерлок Холмс": "Детективы",
+            "Том и Джерри": "Мультфильмы",
+            "Достать ножи": "Детективы",
+            "Друзья": "Комедии",
+        }
+        for book, genre in books_with_genres.items():
+            collector.add_new_book(book)
+            collector.set_book_genre(book, genre)
+        collector.add_book_in_favorites("Гарри Поттер и дары смерти")
         book_to_delete = "Шерлок Холмс"
-        before_change_favorites = len(collector_with_favorites.favorites)
-        collector_with_favorites.delete_book_from_favorites(book_to_delete)
-        after_change_favorites = len(collector_with_favorites.favorites)
+        before_change_favorites = len(collector.favorites)
+        collector.delete_book_from_favorites(book_to_delete)
+        after_change_favorites = len(collector.favorites)
         assert after_change_favorites == before_change_favorites
 
     # тестируем get_list_of_favorites_books - получение списка книг, добавленных в favorites
